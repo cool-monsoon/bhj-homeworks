c02d703240e1c2b836b71d2b81394769f37c01bd
Ольга Храпкова <cool_monsoon@mail.ru>
2022-10-06 20:58:30 +0300

    Task 4.1 fixed

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,13 +2,12 @@ const reveal = Array.from(document.querySelectorAll('.reveal'));
 
 function isVisible(element) {
 	const { top, bottom } = element.getBoundingClientRect();
-	if (bottom < 0) {
+	if (top > 0 && bottom < window.innerHeight) {
+		element.classList.add('reveal_active');
+	} else {
 		element.classList.remove('reveal_active');
 	}
-	if (top > window.innerHeight) {
-		element.classList.remove('reveal_active');
-	}
-	element.classList.add('reveal_active');
+
 }
 
 window.addEventListener('scroll', () => { reveal.forEach((element) => { isVisible(element) }) });
